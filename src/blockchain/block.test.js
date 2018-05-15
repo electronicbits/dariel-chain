72b9b72bfc99472946d91675bf63bf42ff2e75bd
Dario Alvarez <dario@electronic-bits.com>
2018-05-15 21:29:09 +1000

    proof of work with difficulty and unit test passing

--- src/blockchain/block.test.js
+++ src/blockchain/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { DIFFICULTY } = require('../../config');
 
 describe('Block',() => {
 
@@ -18,5 +19,7 @@ describe('Block',() => {
         expect(block.lastHash).toEqual(lastBlock.hash);
     });
 
-    
+    it('generates a hash that matches the difficulty',() => {
+        expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+    });
 });
