676585f480e57148b7952f7a64025d20eb983d2f
Dario Alvarez <dario@electronic-bits.com>
2018-05-15 21:37:39 +1000

    tests running

--- src/blockchain/block.test.js
+++ src/blockchain/block.test.js
@@ -21,5 +21,6 @@ describe('Block',() => {
 
     it('generates a hash that matches the difficulty',() => {
         expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        console.log(block.toString());
     });
 });
