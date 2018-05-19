01bae8ffea129a175a35cf5c57516a85678b5db6
Dario Alvarez <dario@electronic-bits.com>
2018-05-19 22:28:51 +1000

    dynamic block difficulty

--- src/blockchain/block.test.js
+++ src/blockchain/block.test.js
@@ -1,5 +1,4 @@
 const Block = require('./block');
-const { DIFFICULTY } = require('../../config');
 
 describe('Block',() => {
 
@@ -13,14 +12,18 @@ describe('Block',() => {
 
     it('sets the data to match the input', () => {
         expect(block.data).toEqual(data);
-    })
+    });
 
     it('sets the lastHash to match the hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
     });
 
     it('generates a hash that matches the difficulty',() => {
-        expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
-        console.log(block.toString());
+        expect(block.hash.substring(0, block.difficulty)).toEqual('0'.repeat(block.difficulty));
+        // console.log(block.toString());
     });
+
+    it('lowers the difficulty for slowly mined blocks', () => {
+        expect(Block.adjustDifficulty(block, block.timestamp+360000)).toEqual(block.difficulty-1);
+    })
 });
