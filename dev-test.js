04e5c90d475525973bfdfcbf4a5cb7de23bd5947
Dario Alvarez <dario@electronic-bits.com>
2018-05-19 22:36:22 +1000

    added tests

--- dev-test.js
+++ dev-test.js
@@ -1,8 +1,16 @@
-const Block = require('./src/block');
+// const Block = require('./src/block');
 
-const block = new Block('foo', 'bar', 'zoo', 'baz');
-console.log(block.toString());
-console.log(Block.genesis().toString());
+// const block = new Block('foo', 'bar', 'zoo', 'baz');
+// console.log(block.toString());
+// console.log(Block.genesis().toString());
 
-const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
-console.log(fooBlock.toString());
+// const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+// console.log(fooBlock.toString());
+
+const Blockchain = require('./src/blockchain');
+
+const bc = new Blockchain();
+
+for (let i=0; i<10; i++) {
+    console.log(bc.addBlock(`foo ${i}`).toString());
+}
